064a702ffcdc41e45edca884a445aa2f6aa35c76
kokiabeven <cbucmlgr1@gmail.com>
2024-06-12 21:33:07 +0900

    terraform settings

--- infra/terraform.tfvars
+++ infra/terraform.tfvars
@@ -1,1 +1,2 @@
 vercel_api_token = "6ad8KFzWbBUOCZ7b5BcLyAbd"
+env_example="aiueoaaaaaaaaaaaaaaaaaaa"
---
+++ infra/vercel.tf
@@ -0,0 +1,36 @@
+terraform {
+  required_providers {
+    vercel = {
+      source = "vercel/vercel"
+    }
+  }
+}
+
+provider "vercel" {
+  api_token = var.vercel_api_token
+}
+
+variable "vercel_api_token" {}
+variable "env_example" {}
+
+resource "vercel_project" "rasajapan-fe" {
+  name            = "rasajapan-fe"
+  framework       = "nextjs"
+  build_command   = "yarn build"
+  team_id         = "team_cUUJOF5e58em9YTg9UnbzH1n"
+  install_command = "yarn"
+  git_repository = {
+    type              = "github"
+    repo              = "wrexiv/rasajapan-frontend"
+    branch            = "main"
+    production_branch = "main"
+  }
+  serverless_function_region = "hnd1"
+  environment = [
+    {
+      key    = "ENV_EXAMPLE"
+      value  = var.env_example
+      target = ["production", "preview"]
+    },
+  ]
+}
